9c32f6be16de374ac153fe966ebd6801073c53b6
OlgaVolos <72765623+OlgaVolos@users.noreply.github.com>
2023-03-15 11:38:26 +0200

    lesson 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import {useState} from "react";
+
+const Display = ({counter}) => (
+        <div>
+            <h1>{counter}</h1>
+        </div>
+    )
+
+const Button = ({handleClick, text}) => (
+        <>
+            <button onClick={handleClick}>{text}</button>
+        </>
+    )
+
+
 
 function App() {
-  return (
-    <div className="App">
+ const [counter, setCounter] = useState(0)
 
+    console.log('rendering with counter', counter)
+
+    const increaseByOne = () => setCounter(counter+1);
+    const decreaseByOne = () => setCounter(counter-1);
+    const setToZero = () => setCounter(0)
+
+
+    return (
+    <div>
+        <Display counter={counter}/>
+        <Button handleClick={increaseByOne} text={'plus'}/>
+        <Button handleClick={decreaseByOne} text={'minus'}/>
+        <Button handleClick={setToZero} text={'zero'}/>
+        <Button handleClick={()=> setCounter(counter+10)} text={'+10'}/>
     </div>
   );
 }
